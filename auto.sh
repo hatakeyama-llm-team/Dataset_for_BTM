2c1a52f0d8acb614716cd3b1602610c4a28c7707
Your Name <your.email@example.com>
2024-03-19 08:37:59 +0900

    auto

---
+++ auto.sh
@@ -0,0 +1,13 @@
+conda activate textprocess
+cd codes
+
+#分類モデルの訓練
+python train_classifier.py
+
+#分類モデルを用いてファイルを分類
+rm -rf ../data/categorized #必要に応じて初期化
+rm -rf temp/fin   #終了済みファイルリストを必要に応じて初期化
+python clean_and_clustering.py 16 # 数学は並列処理の数
+
+#dedup
+python dedup.py 16 # 数値は並列処理の数
