148692c9b8aabc88767a45879ba267b7e6204bea
Your Name <your.email@example.com>
2024-04-13 15:12:09 +0900

    upload

--- 01web_codes/7_upload.py
+++ 01web_codes/7_upload.py
@@ -16,15 +16,9 @@ n_clusters = 10000
 
 done_list_path = "../data/temp/done_list.txt"
 
-# done_list = []
-# if os.path.exists(done_list_path):
-#    with open(done_list_path, "r") as f:
-#        done_list = f.read().split("\n")
-
 
 def load(cluster_id):
-    # if str(cluster_id) in done_list:
-    #    return []
+
     files = glob.glob(f"../data/dedup_categorized/{cluster_id}/*.jsonl")
     if len(files) == 0:
         return []
@@ -38,33 +32,26 @@ def load(cluster_id):
     return text_list
 
 
-def upload(cluster_id):
+def upload(cnt, text_list):
+    df = pd.DataFrame(text_list, columns=["text"])
+    df.to_parquet(f"../data/temp/temp.parquet")
+    try:
+        print(f"Uploading {cnt}")
+        hf.upload_file(path_or_fileobj="../data/temp/temp.parquet",
+                       path_in_repo=f"parquet_files/{cnt}.parquet",
+                       repo_id="kanhatakeyama/TanukiCorpus", repo_type="dataset")
+        time.sleep(100)
+    except Exception as e:
+        print(e)
 
-    hf.upload_file(path_or_fileobj="../data/temp/temp.parquet",
-                   path_in_repo=f"parquet_files/{cluster_id}.parquet",
-                   repo_id="kanhatakeyama/TanukiCorpus", repo_type="dataset")
 
-    # done_list.append(str(cluster_id))
-    # with open(done_list_path, "w") as f:
-    #    f.write("\n".join(done_list))
-
-
-while True:
-    text_list = []
-    cnt = 0
-    for cluster_id in tqdm(range(n_clusters)):
-        text_list += load(cluster_id)
-        if len(text_list) > 3*10**6:
-            df = pd.DataFrame(text_list, columns=["text"])
-            df.to_parquet(f"../data/temp/temp.parquet")
-            try:
-                print(f"Uploading {cnt}")
-                upload(cnt)
-                time.sleep(100)
-            except Exception as e:
-                print(e)
-                # time.sleep(1)
-            text_list = []
-            cnt += 1
-    break
-    time.sleep(3600*24)
+text_list = []
+cnt = 0
+for cluster_id in tqdm(range(n_clusters)):
+    text_list += load(cluster_id)
+    if len(text_list) > 3*10**6:
+        upload(cnt, text_list)
+        text_list = []
+        cnt += 1
+cnt += 1
+upload(cnt, text_list)
