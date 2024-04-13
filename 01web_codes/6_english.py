3b67a3902c8f6610cc58fd72c6387f504ef1d790
Your Name <your.email@example.com>
2024-04-13 10:56:25 +0900

    count

--- 01web_codes/6_english.py
+++ 01web_codes/6_english.py
@@ -13,7 +13,7 @@ loader = iter(ds)
 
 # %%
 out_path = "/data/hatakeyama/python/eng_corpus/eng3.jsonl"
-
+count = 0
 with open(out_path, "w") as f:
     f.write("")
 
@@ -21,8 +21,10 @@ for i in tqdm(range(n_articles)):
     t = next(loader)["text"]
     if t == "":
         continue
-
+    count += 1
     with open(out_path, "a") as f:
         f.write(json.dumps({"text": t}, ensure_ascii=False)+"\n")
 
+with open(out_path.replace(".jsonl", "_count.txt"), "w") as f:
+    f.write(str(count))
 # %%
