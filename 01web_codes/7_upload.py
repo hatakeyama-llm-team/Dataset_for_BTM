9f878bf6c28dac8f8af4cf7f703249d635719257
Your Name <your.email@example.com>
2024-04-13 12:21:14 +0900

    upload

--- 01web_codes/7_upload.py
+++ 01web_codes/7_upload.py
@@ -1,4 +1,5 @@
 # %%
+from huggingface_hub import HfApi, logging
 import os
 import glob
 import json
@@ -7,51 +8,63 @@ import time
 from tqdm import tqdm
 # os.environ["HF_ENDPOINT"] = "http://localhost:5564"
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
-from huggingface_hub import HfApi, logging
 logging.set_verbosity_debug()
 hf = HfApi()
 
 # %%
-n_clusters=10000
+n_clusters = 10000
 
-done_list_path="../data/temp/done_list.txt"
+done_list_path = "../data/temp/done_list.txt"
 
-done_list=[]
-if os.path.exists(done_list_path):
-    with open(done_list_path, "r") as f:
-        done_list=f.read().split("\n")
+# done_list = []
+# if os.path.exists(done_list_path):
+#    with open(done_list_path, "r") as f:
+#        done_list = f.read().split("\n")
 
-def upload(cluster_id):
-    if str(cluster_id) in done_list:
-        return
-    files=glob.glob(f"../data/dedup_categorized/{cluster_id}/*.jsonl")
-    if len(files)==0:
-        return
-    text_list=[]
+
+def load(cluster_id):
+    # if str(cluster_id) in done_list:
+    #    return []
+    files = glob.glob(f"../data/dedup_categorized/{cluster_id}/*.jsonl")
+    if len(files) == 0:
+        return []
+    text_list = []
     for file in files:
         with open(file, "r") as f:
             for line in f:
                 text_list.append(json.loads(line)["text"])
-    df=pd.DataFrame(text_list, columns=["text"])
-    df.to_parquet(f"../data/temp/temp.parquet")
 
-    
+    text_list = list(set(text_list))
+    return text_list
+
+
+def upload(cluster_id):
+
     hf.upload_file(path_or_fileobj="../data/temp/temp.parquet",
-    path_in_repo=f"parquet_files/{cluster_id}.parquet",
-    repo_id="kanhatakeyama/TanukiCorpus", repo_type="dataset")
+                   path_in_repo=f"parquet_files/{cluster_id}.parquet",
+                   repo_id="kanhatakeyama/TanukiCorpus", repo_type="dataset")
 
-    done_list.append(str(cluster_id))
-    with open(done_list_path, "w") as f:
-        f.write("\n".join(done_list))
+    # done_list.append(str(cluster_id))
+    # with open(done_list_path, "w") as f:
+    #    f.write("\n".join(done_list))
+
 
 while True:
+    text_list = []
+    cnt = 0
     for cluster_id in tqdm(range(n_clusters)):
-        try:
-            upload(cluster_id)
-        except Exception as e:
-            print(e)
-            #time.sleep(1)
-    time.sleep(1000)
+        text_list += load(cluster_id)
+        if len(text_list) > 3*10**6:
+            df = pd.DataFrame(text_list, columns=["text"])
+            df.to_parquet(f"../data/temp/temp.parquet")
+            try:
+                print(f"Uploading {cnt}")
+                upload(cnt)
+                time.sleep(100)
+            except Exception as e:
+                print(e)
+                # time.sleep(1)
+            text_list = []
+            cnt += 1
 
-
-
+    time.sleep(3600*24)
